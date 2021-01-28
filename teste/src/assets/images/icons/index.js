caa1d017f58ee4430c568a85f450cddbeacf1652
samuelfranklin <samuel.franklin@ipepar.com>
2021-01-28 20:11:44 -0300

    add icons

---
+++ teste/src/assets/images/icons/index.js
@@ -0,0 +1,5 @@
+export { default as calcIcon } from './calc.svg';
+export { default as chartIcon } from './chart.svg';
+export { default as contactIcon } from './contact.svg';
+export { default as newDocumentIcon } from './newDocument.svg';
+export { default as solIcon } from './sol.svg';
